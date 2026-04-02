bf14ce5f1aebfe4b5eeea5c16eb89b7b0af54e04
dangthailinh <dangthailinh2004@gmail.com>
2026-04-02 12:04:23 +0700

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -17,14 +17,7 @@ const sourcePath = path.join(__dirname, './src');
 
 // Common plugins
 const plugins = [
-	// new webpack.optimize.CommonsChunkPlugin({
-	// 	name: 'vendor',
-	// 	filename: 'vendor.js',
-	// 	minChunks(module) {
-	// 		const context = module.context;
-	// 		return context && context.indexOf('node_modules') >= 0;
-	// 	},
-	// }),
+
 	new webpack.DefinePlugin({
 		'process.env': {
 			NODE_ENV: JSON.stringify(nodeEnv),
